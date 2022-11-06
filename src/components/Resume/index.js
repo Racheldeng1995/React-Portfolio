8ddf8e4cbfee8d4fa0ae6726efcc450cea0e2e19
Racheldeng1995 <racheldeng1995@gmail.com>
2022-11-06 10:51:23 -0800

    add resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -3,7 +3,7 @@ function Resume() {
   return (
     <section className="my-5">
       <h1 id="resume">Resume</h1>
-      <a href='../../../public/Resume_Rachel.pdf' download>Downloadable Resume</a>
+      <a href='../../../assets/Resume_Rachel.pdf' download>Downloadable Resume</a>
       <div className="my-2">
         <h1> Tech Stack</h1>
         <div className="my-2">
